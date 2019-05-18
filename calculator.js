d61ed82cd626b2ed16719cb29fd46c8f26654f03
Joanna Klimas <klimas.j.m@gmail.com>
2019-05-19 09:47:36 +1200

    remove debuggers

--- calculator.js
+++ calculator.js
@@ -6,7 +6,6 @@ $("button").click(function() {
   var fired_button = $(this).val();
   var number = Number(fired_button);
   var display = document.querySelector("#display");
-debugger
  
   if (fired_button == "AC") ac(); 
   else if (fired_button == "CE") ce();
@@ -24,20 +23,17 @@ debugger
 
 
 function ac() {
-  debugger
   restart();
   total = 0;
   display.value = total;
 }
 
 function ce() {
-  debugger
   entries.pop();
   display.value = entries.join("");
 }
 
 function equals() {
-  debugger
   answer();
   display.value = total;
   restart();
@@ -45,39 +41,31 @@ function equals() {
 }
 
 function secondOperator() {
-  debugger
   answer();
   display.value = total;
   restartWithoutEquals();
 }
 
 function isNumber() {
-  debugger
   for (var i = 0; i < entries.length; i++) {
     display.value = entries.join("");
   }
 }
 
 function operator() {
-  debugger
-  // for (var i = 0; i < entries.length; i++) {
   display.value = entries.join("");
-  // }
 }  
 
 function restart() {
-  debugger
   entries = [];
   temp = "";
 }
 
 function answer() {
-  debugger
   total = eval(entries.join(""));
 }
 
 function restartWithoutEquals () {
-  debugger
   entries = [];
   temp = "";
   entries[0] = total;
@@ -85,7 +73,6 @@ function restartWithoutEquals () {
 }
 
 function isThisFirstOperator (arr) {
-  debugger
   for (var i = 1; i < arr.length; i++) {
     if (arr[i] == "*" || arr[i] == "/" || arr[i] == "+" || arr[i] == "-") {
     return false;  
